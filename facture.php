352e1119124527e79dbc08877422b2ae6441f357
Clem <levasseur.clementine@gmail.com>
2019-03-07 14:59:59 +0100

    correction sur facture.php ajout ! sur isset + détails des commentaires

--- facture.php
+++ facture.php
@@ -1,45 +1,47 @@
 <?php
 
 //tests de $_POST et redirection vers le formulaire
+//utilisation de empty afin de test si la $ est vide
+//utilisation de isset affin de test si la $ est définie !!erreur commise: oubli de ! devant isset donc mauvaise interprétation
+//(on veut pour la cond que la $ soit vide ou non set)
+//header renvoie au formulaire si les deux cond sont ok
+//ne pas oublier 'location:...'
 
-   if (empty($_POST['nom']) or isset($_POST['nom']))    
+   if (empty($_POST['nom']) or !isset($_POST['nom']))    
     {
         header('location: /SmartServices/FormulairesPhp/index.php');
     }
  
 
-    if (empty($_POST['adresse']) or isset($_POST['adresse']))   
+    if (empty($_POST['adresse']) or !isset($_POST['adresse']))   
     {
         header('location: /SmartServices/FormulairesPhp/index.php');
     }
 
 
-   if (empty($_POST['description']) or isset($_POST['description']))   
+   if (empty($_POST['description']) or !isset($_POST['description']))   
     {
         header('location: /SmartServices/FormulairesPhp/index.php');
     }
 
-    if (empty($_POST['prix']) or isset($_POST['prix']))   
+    if (empty($_POST['prix']) or !isset($_POST['prix']))   
     {
         header('location: /SmartServices/FormulairesPhp/index.php');
     }
 
 
-    if (empty($_POST['tva']) or isset($_POST['tva']))   
+    if (empty($_POST['tva']) or !isset($_POST['tva']))   
     {
         header('location: /SmartServices/FormulairesPhp/index.php');
     }
 
-//affichage des données
+//affichage des données avec concaténation
     
-    elseif (!empty($_POST) or !isset($_POST) == false)
-    {
-        echo '<p>Vous êtes' . $_POST['nom'] . '</p>', 
-            '<p>Vous habitez' . $_POST['adresse'] . '</p>',
-            '<p>Vous souhaitez' . $_POST['description'] . '</p>',
+        echo '<p>Vous êtes ' . $_POST['nom'] . '</p>', 
+            '<p>Vous habitez ' . $_POST['adresse'] . '</p>',
+            '<p>Vous souhaitez ' . $_POST['description'] . '</p>',
             '<p>Le prix unitaire :' . $_POST['prix'] . '</p>', 
             '<p> La TVA : ' . $_POST['tva'] . '</p>'; 
-    }
 
 
 ?> 
