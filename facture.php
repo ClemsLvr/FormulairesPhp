bbf668feaac6b832ff247cadae23b7bfcca67c14
Clem <levasseur.clementine@gmail.com>
2019-03-07 14:35:41 +0100

    Déplacé header en haut + ajout commentaires

--- facture.php
+++ facture.php
@@ -1,59 +1,46 @@
 <?php
-      
-   if (empty($_POST['nom']) == false or isset($_POST['nom']) == false)
-    {
-    echo $_POST['nom'] . '<br>'; 
-    }
 
-    elseif (empty($_POST['nom']) or isset($_POST['nom']))    
+//tests de $_POST et redirection vers le formulaire
+
+   if (empty($_POST['nom']) or isset($_POST['nom']))    
+    {
+        header('location: /SmartServices/FormulairesPhp/index.php');
+    }
+ 
+
+    if (empty($_POST['adresse']) or isset($_POST['adresse']))   
     {
         header('location: /SmartServices/FormulairesPhp/index.php');
     }
 
 
-    if (empty($_POST['adresse']) == false or isset($_POST['adresse']) == false)
+   if (empty($_POST['description']) or isset($_POST['description']))   
     {
-    echo $_POST['adresse'] . '<br>'; 
+        header('location: /SmartServices/FormulairesPhp/index.php');
     }
 
-    elseif (empty($_POST['adresse']) or isset($_POST['adresse']))   
+    if (empty($_POST['prix']) or isset($_POST['prix']))   
     {
         header('location: /SmartServices/FormulairesPhp/index.php');
     }
 
 
-    if (empty($_POST['description']) == false or isset($_POST['description']) == false)
-    {
-    echo $_POST['description'] . '<br>'; 
-    }
-
-    elseif (empty($_POST['description']) or isset($_POST['description']))   
+    if (empty($_POST['tva']) or isset($_POST['tva']))   
     {
         header('location: /SmartServices/FormulairesPhp/index.php');
     }
 
-
-    if (empty($_POST['prix']) == false or isset($_POST['prix']) == false)
-    {
-    echo $_POST['prix'] . '<br>'; 
-    }
-
-    elseif (empty($_POST['prix']) or isset($_POST['prix']))   
-    {
-        header('location: /SmartServices/FormulairesPhp/index.php');
-    }
-
-
-     if (empty($_POST['tva']) == false or isset($_POST['tva']) == false)
-    {
-    echo $_POST['tva'] . '<br>'; 
-    }
-
-    elseif (empty($_POST['tva']) or isset($_POST['tva']))   
-    {
-        header('location: /SmartServices/FormulairesPhp/index.php');
-    }
+//affichage des données
     
+    elseif (!empty($_POST) or !isset($_POST) == false)
+    {
+        echo '<p>Vous êtes' . $_POST['nom'] . '</p>', 
+            '<p>Vous habitez' . $_POST['adresse'] . '</p>',
+            '<p>Vous souhaitez' . $_POST['description'] . '</p>',
+            '<p>Le prix unitaire :' . $_POST['prix'] . '</p>', 
+            '<p> La TVA : ' . $_POST['tva'] . '</p>'; 
+    }
+
 
 ?> 
 
