fcef8f3111fb0a61be6d6885dbb5c0b18e9234f5
Clem <levasseur.clementine@gmail.com>
2019-03-08 16:56:06 +0100

    ajout de include pour sortir les fonctions

--- facture.php
+++ facture.php
@@ -11,6 +11,7 @@
 //var_dump($_FILES); die();
 
 $uploads_dir = './upload/';
+//indique la direction depuis l'emplacement actuel, selon ne pas oublier le .
 $name=$_FILES['logo']['name'];
 $tmp_name=$_FILES['logo']['tmp_name'];
 
@@ -18,43 +19,10 @@ $tmp_name=$_FILES['logo']['tmp_name'];
 
     echo'<img src= "upload/' . $_FILES['logo']['name'] . '"/>';   
     
-
-    
     $champs = ["nom","adresse","description","prix","tva"];
 // => array('nom', 'adresse', 'description','prix', 'tva');
 
-    function checkFormlsValid($champs) 
-//($champs)= les paramètres
-//on définit d'abord la variable
-    {
-        foreach($champs as $element) 
-//foreach (pour chaque) boucle qui va prendre chaque $champs du tableau
-        {
-            if (empty($_POST[$element]) or !isset($_POST[$element]))
-//utilisation de empty afin de test si la $ est vide
-//utilisation de isset affin de test si la $ est définie !!erreur commise: oubli de ! devant isset donc mauvaise interprétation
-//(on veut pour la cond que la $ soit vide or non set)
-            {
-               header('location: /SmartServices/FormulairesPhp/index.php');
-//header renvoie au formulaire si les deux cond sont vérifiées
-//ne pas oublier 'location:...'
-            }
-        }
-    }
-
-    checkFormlsValid($champs);
-//ne pas oublier d'appeler la variable préalablement définie
-
-    function ttcPrice($prix, $tva)
-    {
-        return $prix * (1+($tva)/100);
-//ne pas oublier return pour afficher
-    }
-
-    function tvaPrice($prix, $tva)
-    {
-        return $prix * ($tva/100);
-    }
+    include ('functions.inc.php');
 
     echo '<p>Vous êtes ' . $_POST['nom'] . '</p>',
 
