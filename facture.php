77162c5b9499f3603ad6384b0c97b0a40b1cb2ed
Clem <levasseur.clementine@gmail.com>
2019-03-08 17:34:21 +0100

    déplacé déf de  dans functions.inc.php

--- facture.php
+++ facture.php
@@ -1,14 +1,7 @@
 <?php
 
-// à retenir var_dump($_POST); die(); pour afficher les infos de la $
-
-
-//$_FILES['icone']['name']     Le nom original du fichier, comme sur le disque du visiteur (exemple : mon_icone.png).
-//$_FILES['icone']['type']     Le type du fichier. Par exemple, cela peut être « image/png ».
-//$_FILES['icone']['size']     La taille du fichier en octets.
-//$_FILES['icone']['tmp_name'] L'adresse vers le fichier uploadé dans le répertoire temporaire.
-//$_FILES['icone']['error']    Le code d'erreur, qui permet de savoir si le fichier a bien été uploadé.
-//var_dump($_FILES); die();
+require ('functions.inc.php');
+//on a sorti les fonctions 
 
 $uploads_dir = './upload/';
 //indique la direction depuis l'emplacement actuel, selon ne pas oublier le .
@@ -19,10 +12,8 @@ $tmp_name=$_FILES['logo']['tmp_name'];
 
     echo'<img src= "upload/' . $_FILES['logo']['name'] . '"/>';   
     
-    $champs = ["nom","adresse","description","prix","tva"];
-// => array('nom', 'adresse', 'description','prix', 'tva');
+    
 
-    include ('functions.inc.php');
 
     echo '<p>Vous êtes ' . $_POST['nom'] . '</p>',
 
