a4c844978bbd3ab8c6a22d9e2189840a256847f4
Clem <levasseur.clementine@gmail.com>
2019-03-08 17:52:47 +0100

    transformation de l'upload en fonction et déplacement dans functions.inc.php

--- facture.php
+++ facture.php
@@ -3,17 +3,6 @@
 require ('functions.inc.php');
 //on a sorti les fonctions 
 
-$uploads_dir = './upload/';
-//indique la direction depuis l'emplacement actuel, selon ne pas oublier le .
-$name=$_FILES['logo']['name'];
-$tmp_name=$_FILES['logo']['tmp_name'];
-
-  move_uploaded_file($tmp_name, $uploads_dir . $name);
-
-    echo'<img src= "upload/' . $_FILES['logo']['name'] . '"/>';   
-    
-    
-
 
     echo '<p>Vous êtes ' . $_POST['nom'] . '</p>',
 
--- functions.inc.php
+++ functions.inc.php
@@ -1,5 +1,22 @@
 <?php
 
+
+$uploads_dir = './upload/';
+//indique la direction depuis l'emplacement actuel, selon l'emplacement ne pas oublier le . devant
+$name=$_FILES['logo']['name'];
+$tmp_name=$_FILES['logo']['tmp_name'];
+
+function uploadFile($uploads_dir, $name, $tmp_name)
+{
+  move_uploaded_file($tmp_name, $uploads_dir . $name);
+
+    echo'<img src= "upload/' . $_FILES['logo']['name'] . '"/>';   
+}
+  
+uploadFile($uploads_dir, $_FILES['logo']['name'], $_FILES['logo']['tmp_name']);
+
+
+
 $champs = ["nom","adresse","description","prix","tva"];
 // => array('nom', 'adresse', 'description','prix', 'tva');
 
